5cb9800213e6b4ad9cdbd13a9a37cc325b4a8831
Marcus <mlim0032@student.monash.edu>
2021-04-28 00:08:22 +0800

    add Bush class

---
+++ src/game/Bush.java
@@ -0,0 +1,19 @@
+package game;
+
+import edu.monash.fit2099.engine.Ground;
+
+import java.util.ArrayList;
+
+/**
+ * A class that represents bush.
+ */
+public class Bush extends Ground {
+
+    /**
+     * Constructor.
+     * All bushes are represented by a 'v' character.
+     */
+    public Bush() {
+        super('v');
+    }
+}
