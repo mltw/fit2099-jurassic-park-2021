d5fcd11ddc8dd7e078fc1f3c9f8a83975000ed95
Marcus <mlim0032@student.monash.edu>
2021-04-27 23:56:26 +0800

    add Allosaur class

---
+++ src/game/Allosaur.java
@@ -0,0 +1,62 @@
+package game;
+
+import edu.monash.fit2099.engine.*;
+import edu.monash.fit2099.interfaces.DinosaurInterface;
+
+/**
+ * A carnivore dinosaur.
+ */
+public class Allosaur extends Actor implements DinosaurInterface {
+
+    // Will need to change this to a collection if Allosaur gets additional Behaviours.
+    private Behaviour behaviour;
+
+    /**
+     * Constructor.
+     * All Allosaurs are represented by an 'a' and have 100 hit points.
+     *
+     * @param name the name of the Allosaur
+     */
+    public Allosaur(String name) {
+        super(name, 'a', 100); //to be edited
+
+        behaviour = new WanderBehaviour();
+    }
+
+
+
+    @Override
+    public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
+        return null;
+    }
+
+    @Override
+    public int getFoodLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getUnconsciousCount() {
+        return 0;
+    }
+
+    @Override
+    public int getPregnantCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isPregnant() {
+        return false;
+    }
+
+    @Override
+    public void eat(Item item) {
+
+    }
+
+    @Override
+    public void breed() {
+
+    }
+}
