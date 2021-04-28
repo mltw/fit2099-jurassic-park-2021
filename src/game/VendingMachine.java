4d29f24a7e7888ea73f71f92ff13fcab762793ac
Marcus <mlim0032@student.monash.edu>
2021-04-28 16:32:56 +0800

    add VendingMachine class

---
+++ src/game/VendingMachine.java
@@ -0,0 +1,17 @@
+package game;
+
+import edu.monash.fit2099.engine.Ground;
+
+/**
+ * A class for a vending machine. Only one vending machine in a game.
+ */
+public class VendingMachine extends Ground {
+
+    /**
+     * Constructor.
+     * A vending machine is displayed by a char '8'
+     */
+    public VendingMachine() {
+        super('8');
+    }
+}
