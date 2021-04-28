965a2b5a1e22a6617147e75aa53ae6fc2ceb422d
Marcus <mlim0032@student.monash.edu>
2021-04-29 00:56:27 +0800

    update BreedAction class

--- src/game/BreedAction.java
+++ src/game/BreedAction.java
@@ -4,10 +4,26 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
+/**
+ * Action for dinosaurs to breed with each other.
+ */
 public class BreedAction extends Action {
+    Actor current;
+    Actor target;
+
+    public BreedAction(Actor current, Actor target) {
+
+    }
+
+    /**
+     *
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
-        return null;
+        return menuDescription(actor);
     }
 
     @Override
@@ -15,3 +31,20 @@ public class BreedAction extends Action {
         return null;
     }
 }
+//    @Override
+//    public String execute(Actor actor, GameMap map) {
+//        actor.removeItemFromInventory(item);
+//        map.locationOf(actor).addItem(item);
+//        return menuDescription(actor);
+//    }
+//
+//    /**
+//     * A string describing the action suitable for displaying in the UI menu.
+//     *
+//     * @param actor The actor performing the action.
+//     * @return a String, e.g. "Player drops the potato"
+//     */
+//    @Override
+//    public String menuDescription(Actor actor) {
+//        return actor + " drops the " + item;
+//    }
