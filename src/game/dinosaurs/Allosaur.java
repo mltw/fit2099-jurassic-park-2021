539549c3fa0a0d7cde41dcd862cf6c9250f29763
Marcus <mlim0032@student.monash.edu>
2021-04-29 22:06:09 +0800

    update Allosaur class

--- src/game/dinosaurs/Allosaur.java
+++ src/game/dinosaurs/Allosaur.java
@@ -1,35 +1,31 @@
 package game.dinosaurs;
 
 import edu.monash.fit2099.engine.*;
-import edu.monash.fit2099.interfaces.DinosaurInterface;
 import game.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * A carnivore dinosaur.
  */
-public class Allosaur extends Actor implements DinosaurInterface {
+public class Allosaur extends Dinosaur {
 
-    // Will need to change this to a collection if Allosaur gets additional Behaviours.
-    private Behaviour behaviour;
-    private int unconsciousCount;
-    private int pregnantCount;
-    private String gender;
-    private ArrayList<String> cantAttack;
+    //    private ArrayList<String> cantAttack;
+    private HashMap<String, Integer> cantAttack;
+    // used to give a unique name for each Allosaur
+    private static int allosaurCount = 1;
 
     /**
      * Constructor.
      * All Allosaurs are represented by an 'a' and have 100 hit points.
-     *
-     * @param name the name of the Allosaur
+     * Their name is represented as "Allosaur" followed by a unique number, eg "Allosaur1".
      */
-    public Allosaur(String name) {
-        super(name, 'a', 100);
+    public Allosaur() {
+        super("Allosaur"+allosaurCount, 'a', 100);
 
-        behaviour = new WanderBehaviour();
-        this.unconsciousCount = 0;
+        allosaurCount++;
 
     }
 
@@ -40,105 +36,144 @@ public class Allosaur extends Actor implements DinosaurInterface {
 
         Location here = map.locationOf(this);
 
-        // NEW VERSION TO FIND ADJACENT AND NEARBY STUFF IDK 29/4/21
-        for (Exit exit: here.getExits()){ //for each possible exit for the allosaur to go to
+
+        // NEW VERSION TO FIND ADJACENT AND NEARBY STUFF
+        for (Exit exit : here.getExits()) { //for each possible exit for the allosaur to go to
             Location destination = exit.getDestination(); //this represents each adjacent square of the current allosaur
-                                                        // regardless of whether that square has an Actor/Tree or wtv
+            // regardless of whether that square has an Actor/Tree or wtv
             List<Exit> nearby = destination.getExits(); //all exits of the adjacent square, ie nearby locations
 //            exit.name is like North/North-East etc...
-            if (destination.getDisplayChar() == 'a'){
-                // do stuff
+
+            if (this.getHitPoints() > 50 && !isPregnant()) {
+                // found an adjacent Allosaur
+                if (destination.getDisplayChar() == 'a') {
+                    Allosaur adjcAllosaur = (Allosaur) destination.getActor();
+                    if (!this.isPregnant() && !adjcAllosaur.isPregnant()
+                            && !(this.getGender().equals(adjcAllosaur.getGender()))) {
+                        return new BreedAction(this, adjcAllosaur);
+                    }
+                }
+            } else if (this.getHitPoints() < 90) {
+                // display hungry message
+                display.println("Allosaur at (" + allosaurLocationX + "," + allosaurLocationY + ") is getting hungry!");
+                // if found a Stegosaur
+                if (destination.getDisplayChar() == 'd') {
+                    Stegosaur adjcStegosaur = (Stegosaur) destination.getActor();
+
+                    // if the Stegosaur is not attacked by this Allosaur within the previous 20 turns
+                    if (!this.cantAttack.containsKey(adjcStegosaur.toString())) {
+                        this.cantAttack.put(adjcStegosaur.toString(), 1); //add the Stegosaur into 'cantAttack'
+                        return new AttackAction(adjcStegosaur);
+                    }
+
+                }
             }
+
 
         }
-
-
-        if ((getHitPoints() > 50) && !isPregnant()){
-            ArrayList result = (checkAdjacentAndNearbySquares(allosaurLocationX, allosaurLocationY, map, 'a'));
-            if (result.get(0).equals("Adjacent")){
-                return new BreedAction(this, (Actor) result.get(1));
-            }
-            else if (result.get(0).equals("Nearby")){
-                return new FollowBehaviour((Actor) result.get(1)).getAction(this, map);
-            }
-//            for (int y = allosaurLocationY - 2; y <= (allosaurLocationY + 2) ;y++){
-//                for (int x = allosaurLocationX - 2; x <= (allosaurLocationX + 2) ;x++ ){
-//                    if (map.at(x,y).getActor().getDisplayChar() == 'a'){
-//                        Actor nextAllosaur = map.at(x,y).getActor();
-//                        int nextAllosaurLocationX = map.locationOf(nextAllosaur).x();
-//                        int nextAllosaurLocationY = map.locationOf(nextAllosaur).y();
-//
-//                        // ADJACENT SQUARE:
-//                        // if the other allosaur is at left/right column of current allosaur,
-//                        // check if they're on same/upper/lower row
-//                        if ((nextAllosaurLocationX == allosaurLocationX - 1) ||
-//                            (nextAllosaurLocationX == allosaurLocationX + 1))
-//                            {
-//                                if((nextAllosaurLocationY == allosaurLocationY + 1) ||
-//                                (nextAllosaurLocationY == allosaurLocationY) ||
-//                                (nextAllosaurLocationY == allosaurLocationY - 1)){
-//                                    return new BreedAction(this, nextAllosaur);
-//                                }
-//                            }
-//                        // if they're same column, check for two upper and lower rows
-//                        else if (nextAllosaurLocationX == allosaurLocationX)
-//                            {
-//                                if ((nextAllosaurLocationY == allosaurLocationY + 1) ||
-//                                    (nextAllosaurLocationY == allosaurLocationY - 1)){
-//                                    return new BreedAction(this, nextAllosaur);
-//                                }
-//                                // NEARBY
-//                                else if ((nextAllosaurLocationY == allosaurLocationY + 2) ||
-//                                        (nextAllosaurLocationY == allosaurLocationY - 2)){
-//                                    return new FollowBehaviour(nextAllosaur).getAction(this, map);
-//                                }
-//                        }
-//
-//                        // NEARBY, NOT ADJACENT (EG TWO DINOSAURS HAVE ONE SPACE BETWEEN THEM)
-//                        else if ((nextAllosaurLocationX == allosaurLocationX - 2) ||
-//                                (nextAllosaurLocationX == allosaurLocationX + 2))
-//                        {
-//                            if((nextAllosaurLocationY == allosaurLocationY + 2) ||
-//                                (nextAllosaurLocationY == allosaurLocationY + 1) ||
-//                                (nextAllosaurLocationY == allosaurLocationY) ||
-//                                (nextAllosaurLocationY == allosaurLocationY - 1) ||
-//                                (nextAllosaurLocationY == allosaurLocationY - 2)){
-//                                return new FollowBehaviour(nextAllosaur).getAction(this, map);
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-        }
-//        ==============================NOT COMPLETE====================================
-        else if (getHitPoints() < 90){
-            // display hungry message
-            display.println("Allosaur at (" + allosaurLocationX + "," + allosaurLocationY + ") is getting hungry!");
-
-            ArrayList result = (checkAdjacentAndNearbySquares(allosaurLocationX, allosaurLocationY, map, 'd'));
-            Stegosaur toBeAttackedStegosaur = (Stegosaur) result.get(1);
-
-            if (result.get(0).equals("Adjacent") && !cantAttack.contains(((Actor) result.get(1)).toString())){
-                //attack
-                this.hitPoints += 20;
-                if (toBeAttackedStegosaur.getHitPoints() > 0)
-                    this.cantAttack.add(toBeAttackedStegosaur.toString());
-
-                return new AttackAction(toBeAttackedStegosaur);
-            }
-            else{
-//                ArrayList result2 = (checkAdjacentAndNearbySquares(allosaurLocationX, allosaurLocationY, map, '%'));
-
-            }
-        }
-
-
-
-        Action wander = behaviour.getAction(this, map);
+        Action wander = getBehaviour().get(0).getAction(this, map);
         if (wander != null)
             return wander;
 
         return new DoNothingAction();
+
+    }
+
+//        if ((getHitPoints() > 50) && !isPregnant()){
+//            ArrayList result = (checkAdjacentAndNearbySquares(allosaurLocationX, allosaurLocationY, map, 'a'));
+//            if (result.get(0).equals("Adjacent")){
+//                return new BreedAction(this, (Actor) result.get(1));
+//            }
+//            else if (result.get(0).equals("Nearby")){
+//                return new FollowBehaviour((Actor) result.get(1)).getAction(this, map);
+//            }
+////            for (int y = allosaurLocationY - 2; y <= (allosaurLocationY + 2) ;y++){
+////                for (int x = allosaurLocationX - 2; x <= (allosaurLocationX + 2) ;x++ ){
+////                    if (map.at(x,y).getActor().getDisplayChar() == 'a'){
+////                        Actor nextAllosaur = map.at(x,y).getActor();
+////                        int nextAllosaurLocationX = map.locationOf(nextAllosaur).x();
+////                        int nextAllosaurLocationY = map.locationOf(nextAllosaur).y();
+////
+////                        // ADJACENT SQUARE:
+////                        // if the other allosaur is at left/right column of current allosaur,
+////                        // check if they're on same/upper/lower row
+////                        if ((nextAllosaurLocationX == allosaurLocationX - 1) ||
+////                            (nextAllosaurLocationX == allosaurLocationX + 1))
+////                            {
+////                                if((nextAllosaurLocationY == allosaurLocationY + 1) ||
+////                                (nextAllosaurLocationY == allosaurLocationY) ||
+////                                (nextAllosaurLocationY == allosaurLocationY - 1)){
+////                                    return new BreedAction(this, nextAllosaur);
+////                                }
+////                            }
+////                        // if they're same column, check for two upper and lower rows
+////                        else if (nextAllosaurLocationX == allosaurLocationX)
+////                            {
+////                                if ((nextAllosaurLocationY == allosaurLocationY + 1) ||
+////                                    (nextAllosaurLocationY == allosaurLocationY - 1)){
+////                                    return new BreedAction(this, nextAllosaur);
+////                                }
+////                                // NEARBY
+////                                else if ((nextAllosaurLocationY == allosaurLocationY + 2) ||
+////                                        (nextAllosaurLocationY == allosaurLocationY - 2)){
+////                                    return new FollowBehaviour(nextAllosaur).getAction(this, map);
+////                                }
+////                        }
+////
+////                        // NEARBY, NOT ADJACENT (EG TWO DINOSAURS HAVE ONE SPACE BETWEEN THEM)
+////                        else if ((nextAllosaurLocationX == allosaurLocationX - 2) ||
+////                                (nextAllosaurLocationX == allosaurLocationX + 2))
+////                        {
+////                            if((nextAllosaurLocationY == allosaurLocationY + 2) ||
+////                                (nextAllosaurLocationY == allosaurLocationY + 1) ||
+////                                (nextAllosaurLocationY == allosaurLocationY) ||
+////                                (nextAllosaurLocationY == allosaurLocationY - 1) ||
+////                                (nextAllosaurLocationY == allosaurLocationY - 2)){
+////                                return new FollowBehaviour(nextAllosaur).getAction(this, map);
+////                            }
+////                        }
+////                    }
+////                }
+////            }
+//        }
+//        ==============================NOT COMPLETE====================================
+//        else if (getHitPoints() < 90){
+//            // display hungry message
+//            display.println("Allosaur at (" + allosaurLocationX + "," + allosaurLocationY + ") is getting hungry!");
+//
+//            ArrayList result = (checkAdjacentAndNearbySquares(allosaurLocationX, allosaurLocationY, map, 'd'));
+//            Stegosaur toBeAttackedStegosaur = (Stegosaur) result.get(1);
+//
+//            if (result.get(0).equals("Adjacent") && !cantAttack.contains(((Actor) result.get(1)).toString())){
+//                //attack
+//                this.hitPoints += 20;
+//                if (toBeAttackedStegosaur.getHitPoints() > 0)
+//                    this.cantAttack.add(toBeAttackedStegosaur.toString());
+//
+//                return new AttackAction(toBeAttackedStegosaur);
+//            }
+//            else{
+////                ArrayList result2 = (checkAdjacentAndNearbySquares(allosaurLocationX, allosaurLocationY, map, '%'));
+//
+//            }
+//        }
+
+
+
+
+
+
+
+    /**
+     * Creates and returns an intrinsic weapon.
+     * For Allosaur, its intrinsic weapon would be its sharp, pointy teeth. Thus its verb is "bites",
+     * and it'll deal 20 damage to the Stegosaur attacked.
+     *
+     * @return a freshly-instantiated IntrinsicWeapon
+     */
+    @Override
+    protected IntrinsicWeapon getIntrinsicWeapon() {
+        return new IntrinsicWeapon(20, "bites");
     }
 
     public ArrayList checkAdjacentAndNearbySquares(int allosaurLocationX, int allosaurLocationY, GameMap map,
@@ -204,30 +239,5 @@ public class Allosaur extends Actor implements DinosaurInterface {
     }
 
 
-
-    @Override
-    public int getUnconsciousCount() {
-        return unconsciousCount;
-    }
-
-    @Override
-    public int getPregnantCount() {
-        return pregnantCount;
-    }
-
-    @Override
-    public boolean isPregnant() {
-        return false;
-    }
-
-    @Override
-    public int getHitPoints() {
-        return hitPoints;
-    }
-
-    @Override
-    public String getGender() {
-        return gender;
-    }
 
 }
