dd787a5e86f6bb1347c6a70db83c1ba35447e7cb
kexin <ktan0056@student.monash.edu>
2021-04-29 21:53:47 +0800

    updated Bush, Tree, Fruit class

--- src/game/ground/Bush.java
+++ src/game/ground/Bush.java
@@ -1,6 +1,9 @@
 package game.ground;
 
+import edu.monash.fit2099.engine.Display;
 import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Location;
+import game.portableItems.Fruit;
 
 import java.util.ArrayList;
 
@@ -8,6 +11,7 @@ import java.util.ArrayList;
  * A class that represents bush.
  */
 public class Bush extends Ground {
+    private int foodCount;
 
     /**
      * Constructor.
@@ -15,6 +19,25 @@ public class Bush extends Ground {
      */
     public Bush() {
         super('v');
-        addCapability(Status.DEAD);
+        addCapability(Status.ALIVE); // since it can grow something: 29/4
+    }
+
+    /**
+     * Ground can also experience the joy of time.
+     *
+     * @param location The location of the Ground
+     */
+    @Override
+    public void tick(Location location) {
+        super.tick(location);
+
+        // 29/4
+        if(Math.random() == 0.1){
+            // any turn, 10% to produce 1 ripe fruit
+            Fruit item = new Fruit("fruit" , 'f',false);
+            foodCount++;
+            location.addItem(item);
+
+        }
     }
 }
--- src/game/ground/Tree.java
+++ src/game/ground/Tree.java
@@ -1,10 +1,15 @@
 package game.ground;
 
+import edu.monash.fit2099.engine.Display;
 import edu.monash.fit2099.engine.Ground;
 import edu.monash.fit2099.engine.Location;
+import game.portableItems.Fruit;
+
+import java.util.Random;
 
 public class Tree extends Ground {
 	private int age = 0;
+	private int foodCount = 0;
 
 	public Tree() {
 
@@ -21,5 +26,13 @@ public class Tree extends Ground {
 			displayChar = 't';
 		if (age == 20)
 			displayChar = 'T';
+
+		// 29/4
+		if(Math.random() == 0.5){
+			// any turn, 50% to produce 1 ripe fruit(still on tree)
+			Fruit item = new Fruit("fruit" , 'f',false);
+			foodCount++;
+			location.addItem(item); // add item onto this square
+		}
 	}
 }
