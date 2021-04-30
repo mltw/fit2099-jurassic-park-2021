225dcd798154033cc9679affbeef25c3ae63a4d0
Marcus <mlim0032@student.monash.edu>
2021-04-30 14:18:59 +0800

    add LayEggAction class to handle lay egg action

---
+++ src/game/LayEggAction.java
@@ -0,0 +1,34 @@
+package game;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import game.dinosaurs.Dinosaur;
+import game.portableItems.Egg;
+import game.portableItems.EggType;
+
+public class LayEggAction extends Action {
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        ((Dinosaur) actor ).setPregnant(false);
+        ((Dinosaur) actor ).setPregnantCount(0);
+
+        if (actor.getDisplayChar() == 'a'){
+            map.locationOf(actor).addItem(new Egg(EggType.ALLOSAUR));
+        }
+        else if (actor.getDisplayChar() == 'b'){
+            map.locationOf(actor).addItem(new Egg(EggType.BRACHIOSAUR));
+        }
+        else if (actor.getDisplayChar() == 'd'){
+            map.locationOf(actor).addItem(new Egg(EggType.STEGOSAUR));
+        }
+
+        return menuDescription(actor) + map.locationOf(actor).x() + "," +map.locationOf(actor).y() +")!";
+
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + "just laid an egg at (" ;
+    }
+}
