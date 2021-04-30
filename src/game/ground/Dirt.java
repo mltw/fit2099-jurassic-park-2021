3e08e638806d1b5f4609bb271231c3a28cac7f95
kexin <ktan0056@student.monash.edu>
2021-05-01 01:00:29 +0800

    Updated DinosaurLocation & Dirt class

--- src/game/ground/Dirt.java
+++ src/game/ground/Dirt.java
@@ -11,6 +11,7 @@ import java.util.Random;
  * A class that represents bare dirt.
  */
 public class Dirt extends Ground {
+	private boolean read = false;
 
 	public Dirt() {
 
@@ -24,13 +25,24 @@ public class Dirt extends Ground {
 	 * @param location The location of the Ground
 	 */
 	@Override
+	// update: 30/4
 	public void tick(Location location) {
 		super.tick(location);
-//		// each square of dirt has a 0.5% chance to grow a bush
-//		boolean growBushSmallPossibility = new Random().nextInt(200) == 0;
-//		// any square of dirt next to >=2 bushes have 10% chance to grow a bush
-//		boolean growBushPossibility = new Random().nextInt(10) == 0;
-//
+		read = !read;
+		if (read) {
+			int aliveBushNeighbours = aliveBushCount(location);
+			int aliveTreeNeighbours = aliveTreeCount(location);
+
+			// each square of dirt has a 0.5% chance to grow a bush
+			boolean growBushSmallPossibility = new Random().nextInt(200) == 0;
+			// any square of dirt next to >=2 bushes have 10% chance to grow a bush
+			boolean growBushPossibility = new Random().nextInt(10) == 0;
+			if ((aliveBushNeighbours >= 2 && aliveTreeNeighbours < 1) && growBushPossibility) {
+				location.setGround(new Bush());
+			} else if (aliveTreeNeighbours < 1 && growBushSmallPossibility) {
+				location.setGround(new Bush());
+			}
+
 //		int treeCount = 0;
 //		int bushCount = 0;
 //		for (Exit exit : location.getExits()) {
@@ -54,5 +66,17 @@ public class Dirt extends Ground {
 //		else if (treeCount < 1 && growBushSmallPossibility) {
 //			location.setGround(new Bush());
 //		}
+		}
+	}
+	private int aliveBushCount(Location location) {
+		return (int) location.getExits().stream().map(exit -> exit.getDestination().getGround())
+				.filter(ground -> ground.hasCapability(Status.ALIVE) && ground.getDisplayChar()=='v').count();
+	}
+
+	private int aliveTreeCount(Location location) {
+		return (int) location.getExits().stream().map(exit -> exit.getDestination().getGround())
+				.filter(ground -> ground.hasCapability(Status.ALIVE) && ((ground.getDisplayChar() == '+') ||
+						(ground.getDisplayChar() == 't') ||
+						(ground.getDisplayChar() == 'T'))).count();
 	}
 }
