840df6f2ef26164393622c92badba8199f77c77d
kexin <ktan0056@student.monash.edu>
2021-05-01 00:32:07 +0800

    Updated DinosaurLocation

--- src/game/DinosaurLocation.java
+++ src/game/DinosaurLocation.java
@@ -1,9 +1,17 @@
 package game;
 
+import edu.monash.fit2099.demo.conwayslife.ConwayLocation;
+import edu.monash.fit2099.demo.conwayslife.Status;
+import edu.monash.fit2099.engine.Exit;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Location;
+import game.ground.Bush;
+
+import java.util.Random;
 
 public class DinosaurLocation extends Location {
+    private boolean read = false;
+    private DinosaurLocation.NextTurn action = DinosaurLocation.NextTurn.SAME;
 
     public DinosaurLocation(GameMap map, int x, int y) {
         super(map, x, y);
@@ -15,7 +23,37 @@ public class DinosaurLocation extends Location {
      */
     @Override
     public void tick() {
-        super.tick();
+        read = !read;
+        if (read) {
+            int aliveBushNeighbours = aliveBushCount();
+            int aliveTreeNeighbours = aliveTreeCount();
+            boolean aliveHere = getGround().hasCapability(Status.ALIVE);
+            // each square of dirt has a 0.5% chance to grow a bush
+            boolean growBushSmallPossibility = new Random().nextInt(200) == 0;
+            // any square of dirt next to >=2 bushes have 10% chance to grow a bush
+            boolean growBushPossibility = new Random().nextInt(10) == 0;
+            if ((aliveBushNeighbours>=2 && aliveTreeNeighbours<1) && growBushPossibility){
+                setGround(new Bush());
+            }
+            else if(aliveTreeNeighbours <1 && growBushSmallPossibility){
+                setGround(new Bush());
+            }
+            super.tick();
+        }
+    }
+    private int aliveBushCount() {
+        return (int) getExits().stream().map(exit -> exit.getDestination().getGround())
+                .filter(ground -> ground.hasCapability(Status.ALIVE) && ground.getDisplayChar()=='v').count();
+    }
 
+    private int aliveTreeCount() {
+        return (int) getExits().stream().map(exit -> exit.getDestination().getGround())
+                .filter(ground -> ground.hasCapability(Status.ALIVE) && ((ground.getDisplayChar() == '+') ||
+					(ground.getDisplayChar() == 't') ||
+					(ground.getDisplayChar() == 'T'))).count();
+    }
+
+    private enum NextTurn {
+        GROW, DIE, SAME
     }
 }
--- src/game/ground/Dirt.java
+++ src/game/ground/Dirt.java
@@ -26,33 +26,33 @@ public class Dirt extends Ground {
 	@Override
 	public void tick(Location location) {
 		super.tick(location);
-		// each square of dirt has a 0.5% chance to grow a bush
-		boolean growBushSmallPossibility = new Random().nextInt(200) == 0;
-		// any square of dirt next to >=2 bushes have 10% chance to grow a bush
-		boolean growBushPossibility = new Random().nextInt(10) == 0;
-
-		int treeCount = 0;
-		int bushCount = 0;
-		for (Exit exit : location.getExits()) {
-			// check for number of trees in each location's surrounding
-			if ((exit.getDestination().getDisplayChar() == '+') ||
-					(exit.getDestination().getDisplayChar() == 't') ||
-					(exit.getDestination().getDisplayChar() == 'T')) {
-				treeCount += 1;
-			}
-			// check for number of bushes in each location's surrounding
-			else if (exit.getDestination().getDisplayChar() == 'v') {
-				bushCount += 1;
-			}
-		}
-
-		// bush >=2, no trees = 10% chance of growing bush
-		if ((bushCount >= 2 && treeCount < 1) && (growBushPossibility)) {
-			location.setGround(new Bush());
-		}
-		// bush not >=2, but still no trees = 0.5% chance of growing bush
-		else if (treeCount < 1 && growBushSmallPossibility) {
-			location.setGround(new Bush());
-		}
+//		// each square of dirt has a 0.5% chance to grow a bush
+//		boolean growBushSmallPossibility = new Random().nextInt(200) == 0;
+//		// any square of dirt next to >=2 bushes have 10% chance to grow a bush
+//		boolean growBushPossibility = new Random().nextInt(10) == 0;
+//
+//		int treeCount = 0;
+//		int bushCount = 0;
+//		for (Exit exit : location.getExits()) {
+//			// check for number of trees in each location's surrounding
+//			if ((exit.getDestination().getDisplayChar() == '+') ||
+//					(exit.getDestination().getDisplayChar() == 't') ||
+//					(exit.getDestination().getDisplayChar() == 'T')) {
+//				treeCount += 1;
+//			}
+//			// check for number of bushes in each location's surrounding
+//			else if (exit.getDestination().getDisplayChar() == 'v') {
+//				bushCount += 1;
+//			}
+//		}
+//
+//		// bush >=2, no trees = 10% chance of growing bush
+//		if ((bushCount >= 2 && treeCount < 1) && (growBushPossibility)) {
+//			location.setGround(new Bush());
+//		}
+//		// bush not >=2, but still no trees = 0.5% chance of growing bush
+//		else if (treeCount < 1 && growBushSmallPossibility) {
+//			location.setGround(new Bush());
+//		}
 	}
 }
