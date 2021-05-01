2309014638d42099e9aa7160dfefe27dc1924dfb
Marcus <mlim0032@student.monash.edu>
2021-05-01 22:13:17 +0800

    update Tree class probability condition

--- src/game/ground/Tree.java
+++ src/game/ground/Tree.java
@@ -45,7 +45,7 @@ public class Tree extends Ground {
 		boolean read = false;
 //		if(!read){
 		double rand = Math.random();
-		if(rand == 0.5){
+		if(rand >= 0.5){
 			// any turn, 50% to produce 1 ripe fruit(still on tree)
 			Fruit item = new Fruit("fruit" , 'f');
 			foodCount++;
@@ -57,7 +57,7 @@ public class Tree extends Ground {
 		for (Item item : location.getItems()) {
 //			if (item.hasCapability(Status.ON_TREE)){
 			double random= Math.random();
-			if (random == 0.05 && item.hasCapability(Status.ON_TREE)){
+			if (random <= 0.05 && item.hasCapability(Status.ON_TREE)){
 				// any turn, 5%:0.05 for ripe fruits to fall
 				item.removeCapability(Status.ON_TREE); // remove capability of being on tree
 				item.addCapability(Status.ON_GROUND);  // now capability of being on ground
