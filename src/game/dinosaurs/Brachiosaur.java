18b40e71fcbc4285580323bbe58708f4e1b836da
kexin <ktan0056@student.monash.edu>
2021-05-02 00:54:02 +0800

    Added display dead message for Brachiosaur

--- src/game/dinosaurs/Brachiosaur.java
+++ src/game/dinosaurs/Brachiosaur.java
@@ -110,6 +110,8 @@ public class Brachiosaur extends Dinosaur {
             Item corpse = new PortableItem("dead " + this, '%');
             map.locationOf(this).addItem(corpse);
             map.removeActor(this);}
+            display.println(this + "is dead at (" + map.locationOf(this).x() + ","
+                + map.locationOf(this).y() +")");
 
         displayed = false; // reset
         return action;
