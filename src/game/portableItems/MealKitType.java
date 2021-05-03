d62747f8d4c777d9efcdaf1b5c8fab1054dc8ced
kexin <ktan0056@student.monash.edu>
2021-05-04 02:26:40 +0800

    updated javadoc for MealKitType class

--- src/game/portableItems/MealKitType.java
+++ src/game/portableItems/MealKitType.java
@@ -1,5 +1,11 @@
 package game.portableItems;
 
+/** An Enum class was created to indicate the Capabilities of MealKitType
+ * Since the capabilities are known(not going to change), enum is used.
+ * MealKitType can be divided into:
+ * VEGETARIAN: Vegetarian mealkit
+ * CARNIVORE: Carnivore mealkit
+ */
 public enum MealKitType {
     VEGETARIAN, CARNIVORE
 }
