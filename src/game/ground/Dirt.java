4116d75713a7633b51bee91ab12909501ce5d1ca
kexin <ktan0056@student.monash.edu>
2021-05-03 13:32:32 +0800

    updated dirt class

--- src/game/ground/Dirt.java
+++ src/game/ground/Dirt.java
@@ -38,38 +38,12 @@ public class Dirt extends Ground {
 			// any square of dirt next to >=2 bushes have 10% chance to grow a bush
 			boolean growBushPossibility = new Random().nextInt(10) == 0;
 
-//			boolean read = false;
 			if ((aliveBushNeighbours >= 2 && aliveTreeNeighbours < 1) && growBushPossibility) {
 				location.setGround(new Bush());
 			}
-//			else if (aliveTreeNeighbours < 1 && !read) {
 			else if (aliveTreeNeighbours < 1 && growBushSmallPossibility) {
 				location.setGround(new Bush());
 			}
-
-//		int treeCount = 0;
-//		int bushCount = 0;
-//		for (Exit exit : location.getExits()) {
-//			// check for number of trees in each location's surrounding
-//			if ((exit.getDestination().getDisplayChar() == '+') ||
-//					(exit.getDestination().getDisplayChar() == 't') ||
-//					(exit.getDestination().getDisplayChar() == 'T')) {
-//				treeCount += 1;
-//			}
-//			// check for number of bushes in each location's surrounding
-//			else if (exit.getDestination().getDisplayChar() == 'v') {
-//				bushCount += 1;
-//			}
-//		}
-//
-//		// bush >=2, no trees = 10% chance of growing bush
-//		if ((bushCount >= 2 && treeCount < 1) && (growBushPossibility)) {
-//			location.setGround(new Bush());
-//		}
-//		// bush not >=2, but still no trees = 0.5% chance of growing bush
-//		else if (treeCount < 1 && growBushSmallPossibility) {
-//			location.setGround(new Bush());
-//		}
 		}
 	}
 	private int aliveBushCount(Location location) {
