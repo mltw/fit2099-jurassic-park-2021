39c25acce4618d1f50dc2bd67cfdb27875974024
kexin <ktan0056@student.monash.edu>
2021-05-04 02:38:37 +0800

    updated javadoc for Wall class

--- src/game/Wall.java
+++ src/game/Wall.java
@@ -5,15 +5,28 @@ import edu.monash.fit2099.engine.Ground;
 
 public class Wall extends Ground {
 
+	/**
+	 * Constructor
+	 * A wall is represented by '#' in the game map.
+	 */
 	public Wall() {
 		super('#');
 	}
-	
+
+	/**
+	 * Player is not able to enter a wall.
+	 * @param actor the Actor to check
+	 * @return false
+	 */
 	@Override
 	public boolean canActorEnter(Actor actor) {
 		return false;
 	}
-	
+
+	/**
+	 * A wall can used to block thrown objects.
+	 * @return true
+	 */
 	@Override
 	public boolean blocksThrownObjects() {
 		return true;
