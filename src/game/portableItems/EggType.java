239b3f1de290e89e8e87506950151ec383f78c7a
kexin <ktan0056@student.monash.edu>
2021-05-04 02:19:10 +0800

    updated javadoc for Allosaur class

--- src/game/portableItems/EggType.java
+++ src/game/portableItems/EggType.java
@@ -1,5 +1,12 @@
 package game.portableItems;
 
+/** An Enum class was created to indicate the Capabilities of EggType
+ * Since the capabilities are known(not going to change), enum is used.
+ * EggType can be divided into:
+ * STEGOSAUR  : egg of Stegosaur
+ * BRACHIOSAUR: egg of Brachiosaur
+ * ALLOSAUR   : egg of Allosaur
+ */
 public enum EggType {
     STEGOSAUR, BRACHIOSAUR,ALLOSAUR;
 }
