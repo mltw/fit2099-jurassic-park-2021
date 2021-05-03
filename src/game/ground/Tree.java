8d1e260aaa7f6a02fa6c89d9ca589047713c6755
kexin <ktan0056@student.monash.edu>
2021-05-03 13:33:43 +0800

    updated tree class

--- src/game/ground/Tree.java
+++ src/game/ground/Tree.java
@@ -9,10 +9,7 @@ import java.util.Random;
 public class Tree extends Ground {
 	private int age = 0;
 	private int foodCount = 0;
-	private int counter = 0;
 	boolean dropped = false;
-	private NextTurn action = NextTurn.SAME;
-	private boolean read = false;
 
 	public Tree() {
 
@@ -55,31 +52,15 @@ public class Tree extends Ground {
 
 		// 29/4
 		for (Item item : location.getItems()) {
-//			if (item.hasCapability(Status.ON_TREE)){
 			double random= Math.random();
 			if (random <= 0.05 && item.hasCapability(Status.ON_TREE)){
-//			if (random >= 0.5 && item.hasCapability(Status.ON_TREE)){
 				// any turn, 5%:0.05 for ripe fruits to fall
 				item.removeCapability(Status.ON_TREE); // remove capability of being on tree
 				item.addCapability(Status.ON_GROUND);  // now capability of being on ground
 				displayChar = 'F'; 					   // to indicate fruit is on tree
 				item.getDropAction(); 				   // an action to drop this item
 				dropped = true;
-				counter++;
-			}
-			else if (item.hasCapability(Status.ON_GROUND) && counter == 1){
-				read = !read;
-				if (read){
-					counter++;
-				} else {
-					if (counter == 2)
-						// remove from map
-						location.removeItem(item);
-				}
 			}
 		}
-	}
-	private enum NextTurn {
-		GROW, DIE, SAME
 	}
 }
