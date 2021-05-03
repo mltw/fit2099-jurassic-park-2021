525bf438d4d7a2cbde979a159783868c037cb746
kexin <ktan0056@student.monash.edu>
2021-05-04 00:57:43 +0800

    updated javadoc for Status class

--- src/game/ground/Status.java
+++ src/game/ground/Status.java
@@ -1,5 +1,14 @@
 package game.ground;
 
+/**
+ * An Enum class was created to indicate the Capabilities of Ground
+ * Since the capabilities are known(not going to change), enum is used.
+ * The Status can divided into:
+ * - DEAD: unable to grow fruits
+ * - ALIVE: able to grow fruits
+ * - ON_TREE: fruits are on tree
+ * - ON_GROUND: fruits dropped on the ground
+ */
 public enum Status {
     DEAD,ALIVE,ON_TREE,ON_GROUND
 }
