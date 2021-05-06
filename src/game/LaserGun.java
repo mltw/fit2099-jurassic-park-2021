69354e70699d6bb854fadf7cd3ae44c9f5f42e71
Marcus <mlim0032@student.monash.edu>
2021-05-06 21:08:32 +0800

    edit constructor for LaserGun

--- src/game/LaserGun.java
+++ src/game/LaserGun.java
@@ -8,14 +8,10 @@ import edu.monash.fit2099.engine.WeaponItem;
 public class LaserGun extends WeaponItem {
     /**
      * Constructor.
-     *
-     * @param name        name of the item
-     * @param displayChar character to use for display when item is on the ground
-     * @param damage      amount of damage this weapon does
-     * @param verb        verb to use for this weapon, e.g. "hits", "zaps"
+     * A laser gun is displayed by a '~', deals 70 damage.
      */
-    public LaserGun(String name, char displayChar, int damage, String verb) {
-        super(name, displayChar, damage, verb);
+    public LaserGun() {
+        super("laserGun", '~', 70, "zaps");
         this.portable = true;
     }
 }
