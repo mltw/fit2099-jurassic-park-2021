1960cd7ca830f752778a14aa0f02586f011a5cf9
kexin <ktan0056@student.monash.edu>
2021-05-07 10:58:20 +0800

    delete map that is used for testing purposes in Application class

--- src/game/Application.java
+++ src/game/Application.java
@@ -25,10 +25,6 @@ public class Application {
 		World world = new World(new Display());
 
 		FancyGroundFactory groundFactory = new FancyGroundFactory(new Dirt(), new Wall(), new Floor(), new Tree());
-//		List<String> map = Arrays.asList(
-//				"+##.+.",
-//				".....+",
-//				".....+");
 		List<String> map = Arrays.asList(
 		"................................................................................",
 		"................................................................................",
