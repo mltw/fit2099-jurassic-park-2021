cc87385e689310af6b2874b65243395ef877e76e
kexin <ktan0056@student.monash.edu>
2021-05-12 13:46:20 +0800

    added Lake class

---
+++ src/game/ground/Lake.java
@@ -0,0 +1,45 @@
+package game.ground;
+
+import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Location;
+
+public class Lake extends Ground {
+    private int counter = 0;
+    private int sips = 25;
+    /**
+     * Constructor.
+     * All lake are represented by a '~' character.
+     */
+    public Lake() {
+        super('~');
+
+    }
+
+    /**
+     * Ground can also experience the joy of time.
+     *
+     * @param location The location of the Ground
+     */
+    @Override
+    public void tick(Location location) {
+
+        // The amount of water that is added to the lake is calculated
+        // by multiplying the rainfall by 20 sips where the rainfall
+        // is a random value between 0.1 and 0.6 inclusive.
+
+        super.tick(location);
+        counter++;
+
+        // rainfall
+        double max = 0.7; // exclusive
+        double min = 0.1;
+        double rainfall = Math.random() *(max-min) + min;
+
+        // Every 10 turns, probability of 20%, sky might rain which adds water to all lakes
+        double rand = Math.random();
+         if(counter%10==0 && rand <=0.2){
+
+         }
+
+    }
+}
