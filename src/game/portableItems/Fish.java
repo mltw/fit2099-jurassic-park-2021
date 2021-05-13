386cb72f2f4244134eb4951016c9c754d2a0f538
kexin <ktan0056@student.monash.edu>
2021-05-13 11:53:38 +0800

    update javadoc for Fish class

--- src/game/portableItems/Fish.java
+++ src/game/portableItems/Fish.java
@@ -9,23 +9,16 @@ public class Fish extends PortableItem {
     private int foodpoints = 0;
     Display display = new Display();
 
-    // Each lake can also hold up to maximum 25 fish,
-    // each fish providing 5 food points.
-    // Lakes start the game with only 5 fish.
-    // Each turn, there is a probability of 60% for a new fish to be born
-    // (increasing the number of fish of the lake by 1).
-
     public Fish(String name,int count) {
         super(name, 'h');
         addCapability(ItemType.FISH);
-        this.foodpoints = 5*count;
+        this.foodpoints = 5*count;          // each fish providing 5 food points
         this.count = count;
     }
 
-    /**
-     * Inform an Item on the ground of the passage of time.
-     * This method is called once per turn, if the item rests upon the ground.
-     *
+    /** Each lake can hold up to maximum 25 fish
+     *  Each turn, there is a probability of 60% for a new fish to be born
+     *  (increasing the number of fish of the lake by 1)
      * @param currentLocation The location of the ground on which we lie.
      */
     @Override
@@ -35,19 +28,26 @@ public class Fish extends PortableItem {
         double fishProbability = Math.random();
         if (this.getCount()<25 && fishProbability >=0.6){
             this.count++;
-            display.println("Lake has " + count + " fish!"); // testing
+            display.println("Lake has " + count + " fish!");                // testing
         }
         else if (this.getCount()==25){
-            display.println("This lake has reached the maximum of 25 fish!"); // extra
+            display.println("This lake has reached the maximum of 25 fish!"); // testing
         }
     }
 
-
+    /** Getter
+     * Retrieve the number of fish the lake currently has.
+     * @return the number of fish the lake currently has.
+     */
     @Override
     public int getCount() {
         return count;
     }
 
+    /** Setter
+     * Sets the number of fish of the lake.
+     * @param count number of fish of the lake to be updated.
+     */
     @Override
     public void setCount(int count) {
         this.count = count;
