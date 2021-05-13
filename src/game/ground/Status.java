ced3f2a527b25b25cd22d02b4742aa518d795473
kexin <ktan0056@student.monash.edu>
2021-05-13 11:59:25 +0800

    update javadoc for Status class

--- src/game/ground/Status.java
+++ src/game/ground/Status.java
@@ -8,6 +8,10 @@ package game.ground;
  * - ALIVE: able to grow fruits
  * - ON_TREE: fruits are on tree
  * - ON_GROUND: fruits dropped on the ground
+ * - BUSH: type bush
+ * - TREE: type tree
+ * - LAKE: type lake
+ * - DIRT: type dirt
  */
 public enum Status {
     DEAD,ALIVE,ON_TREE,ON_GROUND, BUSH, TREE,LAKE,DIRT
