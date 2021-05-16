4d0e9dcee7f3fa29a052e4feed406540a9544a05
Marcus <mlim0032@student.monash.edu>
2021-05-16 16:56:43 +0800

    added Pterodactyl into DrinkAction

--- src/game/actions/DrinkAction.java
+++ src/game/actions/DrinkAction.java
@@ -5,8 +5,8 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Ground;
 import game.dinosaurs.Dinosaur;
+import game.dinosaurs.Status;
 import game.ground.Lake;
-import game.ground.Status;
 
 /**
  * Action for dinosaurs to drink water from lake.
@@ -18,8 +18,8 @@ public class DrinkAction extends Action {
      * Perform the drinking Action.
      * When this method is called: it means dinosaur found an adjacent lake & it is thirsty
      * Different dinosaur has different water level that it can consume once
-     * Eg: Stegosaur & Allosaur: each sip will increase water level by 30
-     *     Brachiosaur         : each sip will increase water level by 80
+     * Eg: Stegosaur, Allosaur & Pterodactyl: each sip will increase water level by 30
+     *     Brachiosaur                      : each sip will increase water level by 80
      *
      * @param actor The actor performing the action.
      * @param map   The map the actor is on.
@@ -29,7 +29,9 @@ public class DrinkAction extends Action {
     public String execute(Actor actor, GameMap map) {
         String message = "";
 
-        if (actor.hasCapability(game.dinosaurs.Status.STEGOSAUR)|| actor.hasCapability(game.dinosaurs.Status.ALLOSAUR)) {
+        if (actor.hasCapability(game.dinosaurs.Status.STEGOSAUR)
+                || actor.hasCapability(game.dinosaurs.Status.ALLOSAUR)
+                || actor.hasCapability(game.dinosaurs.Status.PTERODACTYL)) {
             drinkVolume = 30;  // water level that can be consumed: 30
             ((Dinosaur) actor).setWaterLevel(drinkVolume+ ((Dinosaur) actor).getWaterLevel());
         }
